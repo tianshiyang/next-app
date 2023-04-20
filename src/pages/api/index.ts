9bd14ec9ae514832ba68913319270e9119fed8f0
tianshiyang <tianshiyang@icourt.cc>
2023-04-20 10:26:32 +0800

    feat: 更改本地API

--- src/pages/api/index.ts
+++ src/pages/api/index.ts
@@ -2,7 +2,7 @@ import request from "@/config/request"
 
 export function getUser(params: any): Promise<any> {
   return request({
-    url: "http://localhost:4000/users/login",
+    url: "http://172.28.6.184:4000/users/login",
     method: "get",
     params
   })
